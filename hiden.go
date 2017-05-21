0c262585d89aa9c7168ca7297bd934b6d5d63b2f
Takehiro SUGITA <sugitak@freee.co.jp>
2017-05-21 18:53:56 +0900

    add: subcommand style

--- hiden.go
+++ hiden.go
@@ -11,9 +11,17 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "hiden"
 	app.Usage = "install and manage binary files from the Internet"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello, world!")
-		return nil
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "install",
+			Aliases: []string{"i"},
+			Usage:   "install new package",
+			Action: func(c *cli.Context) error {
+				fmt.Println("try install here")
+				return nil
+			},
+		},
 	}
 
 	app.Run(os.Args)
