46e109b3e4c381e425ff881a7f8b2659f3934649
Takehiro SUGITA <sugitak@freee.co.jp>
2017-05-21 18:48:29 +0900

    add: basic cli

--- hiden.go
+++ hiden.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli"
 )
 
 func main() {
-	cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "hiden"
+	app.Usage = "install and manage binary files from the Internet"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Hello, world!")
+		return nil
+	}
+
+	app.Run(os.Args)
 }
