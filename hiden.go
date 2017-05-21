39a6b1e145d9435fd192feb646f3e5bb59f8c5b4
Takehiro SUGITA <sugitak@freee.co.jp>
2017-05-22 01:42:33 +0900

    temporary: creating JSON unmarshal points

--- hiden.go
+++ hiden.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/urfave/cli"
@@ -18,11 +20,37 @@ func main() {
 			Aliases: []string{"i"},
 			Usage:   "install new package",
 			Action: func(c *cli.Context) error {
-				fmt.Println("try install here")
+				name := c.Args().Get(0)
+				fmt.Printf("First argument: %s\n", name)
+				github_binary_install(name)
 				return nil
 			},
 		},
 	}
 
 	app.Run(os.Args)
+}
+
+func github_binary_install(name string) error {
+	name = "https://api.github.com/repos/prometheus/prometheus/releases/latest"
+
+	resp, err := http.Get(name)
+	if err != nil {
+		fmt.Printf("Error getting %s:\n\t%s\n", name, err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error getting text body in %s:\n\t%s\n", name, err)
+		return err
+	}
+
+	err := json.Unmarshal()
+	if err != nil {
+		fmt.Printf("Error unmarshalling JSON in %s:\n\t%s\n ", name, err)
+		return err
+	}
+	return nil
 }
